019b013cda32159dfc915e5829ef797629621288
= <mnieto@javer.com.mx>
2017-10-24 16:54:41 -0500

    Esquema Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
+import { AboutusComponent } from './components/aboutus/aboutus.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { TechComponent } from './components/tech/tech.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutusComponent,
+    TechComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: '', redirectTo:'home', pathMatch:'full'},
+      {path: 'aboutus', component: AboutusComponent},
+      {path: 'contact', component: ContactComponent},
+      {path:'tech',component: TechComponent}      
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
